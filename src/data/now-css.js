db57c215a6827e8eb864a750499cc2dd9dc8a83e
xloz <tjwns53@naver.com>
2020-09-05 00:02:25 +0900

    8-15 modify

--- src/data/now-css.js
+++ src/data/now-css.js
@@ -1,235 +1,43 @@
 export const nowcssCollection = {
-	name: "nowcss",
+	name: "Now Css",
 	areas: [
 		{
 			skills: [
 				{
-					name: "Testing Methodologies",
+					name: "Styled Componets",
 					description: {
-						text: "Learn about various software testing methodologies and why it is important to test your code.",
 						links: [
-							["Wikipedia - Software testing", "https://en.wikipedia.org/wiki/Software_testing"],
-							["Software Testing Methodologies", "https://smartbear.com/learn/automated-testing/software-testing-methodologies/"],
-							["Learn The Methods & Tools", "https://www.inflectra.com/ideas/topic/testing-methodologies.aspx"],
+							["5분 만에 알아보는 styled-componets ", "https://medium.com/@olaf.go/5%EB%B6%84%EB%A7%8C%EC%97%90-%EC%95%8C%EC%95%84%EB%B3%B4%EB%8A%94-styled-components-6cb08eeaae71"],
+							["리액트 Styled Componets – 1편 ", "https://velog.io/@taewo/%EB%A6%AC%EC%95%A1%ED%8A%B8-Styled-Components-76jsolbaf8"],
+							["리액트 Styled Componets – 2편 ", "https://velog.io/@taewo/%EB%A6%AC%EC%95%A1%ED%8A%B8-Styled-Componets-2%ED%8E%B8"],
 						]
-					},
-					skills: [
-						{
-							name: "Unit Testing",
-							description: {
-								text: "Learn about unit testing and how it can be used to test individual units of code.",
-								links: [
-									["Unit Testing Tutorial", "https://www.guru99.com/unit-testing-guide.html"],
-									["Wikipedia - Unit testing", "https://en.wikipedia.org/wiki/Unit_testing"],
-									["What is Unit Testing?", "https://www.tutorialspoint.com/software_testing_dictionary/unit_testing.htm"],
-									["The Arrange, Act and Assert (AAA) Pattern", "https://medium.com/@pjbgf/title-testing-code-ocd-and-the-aaa-pattern-df453975ab80"],
-								]
-							},
-						},
-						{
-							name: "Integration Testing",
-							description: {
-								text: "Learn about integration testing and how it can be used to test modules as a group.",
-								links: [
-									["Integration Testing Tutorial", "https://www.guru99.com/integration-testing.html"],
-									["What is Integration Testing?", "https://www.tutorialspoint.com/software_testing_dictionary/integration_testing.htm"],
-									["Integration Testing", "http://softwaretestingfundamentals.com/integration-testing/"],
-								]
-							},
-						},
-						{
-							name: "System Testing",
-							description: {
-								text: "Learn about system testing and how it can be used to test the fully integrated software product.",
-								links: [
-									["System Testing Tutorial", "https://www.guru99.com/system-testing.html"],
-									["What is System Testing?", "https://www.tutorialspoint.com/software_testing_dictionary/system_testing.htm"],
-									["System Testing", "http://softwaretestingfundamentals.com/system-testing/"],
-								]
-							},
-						},
-						{
-							name: "Acceptance Testing",
-							description: {
-								text: "Learn about acceptance testing and how it can be used to test the software for acceptability",
-								links: [
-									["What is User Acceptance Testing?", "https://www.guru99.com/user-acceptance-testing.html"],
-									["Acceptance Testing", "http://softwaretestingfundamentals.com/acceptance-testing/"],
-									["What is Acceptance Testing?", "https://www.tutorialspoint.com/software_testing_dictionary/acceptance_testing.htm"],
-								]
-							},
-						},
-						{
-							name: "Smoke Testing",
-							description: {
-								text: "Learn about smoke testing and how it can be used to ensure that critical functionalities of the program are working fine.",
-								links: [
-									["Sanity Testing Vs Smoke Testing", "https://www.guru99.com/smoke-sanity-testing.html"],
-									["Smoke Testing", "http://softwaretestingfundamentals.com/smoke-testing/"],
-								]
-							},
-						},
-						{
-							name: "Performance Testing",
-							description: {
-								text: "Learn about performance testing and how it can be used to determine the responsiveness and stability of a program.",
-								links: [
-									["Performance Testing Tutorial", "https://www.guru99.com/performance-testing.html"],
-									["Performance Testing", "http://softwaretestingfundamentals.com/performance-testing/"],
-									["Wikipedia - Software performance testing", "https://en.wikipedia.org/wiki/Software_performance_testing"],
-								]
-							},
-						},
-						{
-							name: "Usability Testing",
-							description: {
-								text: "Learn about usability testing and how it can be used to determine if the system is easily usable for the end-user.",
-								links: [
-									["What is Usability Testing?", "https://www.guru99.com/usability-testing.html"],
-									["Usability Testing", "http://softwaretestingfundamentals.com/usability-testing/"],
-									["Wikipedia - Usability testing", "https://en.wikipedia.org/wiki/Usability_testing"],
-								]
-							},
-						},
-						{
-							name: "White Box Testing",
-							description: {
-								text: "Learn about white box testing and how it can be used to test the software's internal structure, design, and coding.",
-								links: [
-									["What is White Box Testing?", "https://www.guru99.com/white-box-testing.html"],
-									["White Box Testing", "http://softwaretestingfundamentals.com/white-box-testing/"],
-								]
-							},
-						},
-						{
-							name: "Black Box Testing",
-							description: {
-								text: "Learn about black box testing and how a test can be carried out without looking at the internal code.",
-								links: [
-									["What is Black Box Testing?", "https://www.guru99.com/black-box-testing.html"],
-									["Black Box Testing", "http://softwaretestingfundamentals.com/black-box-testing/"],
-								]
-							},
-						},
-						{
-							name: "Automated Testing",
-							description: {
-								text: "Learn about automated testing and when it should be applied.",
-								links: [
-									["What is Automated Testing?", "https://smartbear.com/learn/automated-testing/what-is-automated-testing/"],
-									["Automated Testing Tutorial", "https://www.guru99.com/automation-testing.html"],
-								]
-							},
-						},
-						{
-							name: "Manual Testing",
-							description: {
-								text: "Learn about manual testing and when it should be applied.",
-								links: [
-									["Manual Testing Tutorial", "https://www.guru99.com/manual-testing.html"],
-									["Wikipedia - Manual Testing", "https://en.wikipedia.org/wiki/Manual_testing"],
-									["What is Manual Testing?", "https://www.tutorialspoint.com/software_testing_dictionary/manual_testing.htm"],
-								]
-							},
-						},
-					]
+					}
 				},
 				{
-					name: "Continuous integration",
+					name: "CSS Modules",
 					description: {
-						text: "Learn about continuous integration and it can enable iterative software development.",
 						links: [
-							["dev.to - Continuous Integration Explained", "https://dev.to/markoa/continuous-integration-explained-59f9"],
-							["Continuous Integration Tutorial", "https://www.tutorialspoint.com/continuous_integration/index.htm"],
-							["Wikipedia - Continuous integration", "https://en.wikipedia.org/wiki/Continuous_integration"],
+							["CSS Module – 벨로퍼트와 함께하는 모던 리액트", "https://react.vlpt.us/styling/02-css-module.html"],
+						    ["번역 : CSS Modules.", "https://medium.com/@utatti/%EB%B2%88%EC%97%AD-css-modules-8bb6edf4f2f"],
 						]
-					},
+					}
 				},
 				{
-					name: "A/B Testing",
+					name: "Styled JSX",
 					description: {
-						text: "Learn about A/B testing and how it can be used to optimize your web app.",
 						links: [
-							["dev.to - A/B testing: introduction", "https://dev.to/ice_lenor/ab-testing-introduction-3jpd"],
-							["dev.to - When Not To A/B Test", "https://dev.to/ice_lenor/when-not-to-ab-test-4cc7"],
-							["A/B Testing", "https://www.optimizely.com/optimization-glossary/ab-testing/"],
-							["Wikipedia - A/B testing", "https://en.wikipedia.org/wiki/A/B_testing"],
+							["Styling Next.js with Styled JSX (eng)", "https://nextjs.org/blog/styling-next-with-styled-jsx"],
+							["JSX 소개", "https://reactjs-kr.firebaseapp.com/docs/introducing-jsx.html"],
 						]
-					},
+					}	
 				},
 				{
-					name: "Test Runners",
+					name: "Emotion",
 					description: {
-						text: "Learn about test runners and how they can help running tests.",
 						links: [
-							["Classifications of Testing Software", "https://amzotti.github.io/testing/2015/03/16/what-is-the-difference-between-a-test-runner-testing-framework-assertion-library-and-a-testing-plugin/"],
-							["Udacity - Javascript Testing", "https://www.udacity.com/course/javascript-testing--ud549"],
-							["JavaScript Unit Testing Frameworks", "https://www.guru99.com/javascript-unit-testing-frameworks.html"]
-						]
-					},
-					skills: [
-						{
-							name: "Karma",
-							description: {
-								text: "Learn about the Karma test runner.",
-								links: [
-									["Karma", "https://karma-runner.github.io/latest/index.html"],
-									["Getting started with Karma and Mocha", "https://medium.com/@jazcodes/getting-started-with-karma-and-mocha-for-automated-browser-tests-5ebb6cd02edf"],
-								]
-							}
-						},
-						{
-							name: "Mocha",
-							description: {
-								text: "Learn about the Mocha testing framework.",
-								links: [
-									["Mocha", "https://mochajs.org/"],
-									["A quick and complete guide to Mocha testing", "https://blog.logrocket.com/a-quick-and-complete-guide-to-mocha-testing-d0e0ea09f09d/"],
-									["How to test JavaScript with Mocha", "https://codeburst.io/how-to-test-javascript-with-mocha-the-basics-80132324752e"],
-								]
-							}
-						},
-						{
-							name: "Jasmine",
-							description: {
-								text: "Learn about the Jasmine testing frameworks.",
-								links: [
-									["Jasmine", "https://jasmine.github.io/"],
-									["How to set up a testing environment with Jasmine and karma", "https://medium.com/bb-tutorials-and-thoughts/javascript-how-to-set-up-a-testing-environment-with-jasmine-and-karma-58591dd39734"],
-								]
-							}
-						},
-						{
-							name: "Cypress",
-							description: {
-								text: "Learn how to use Cypress for end-to-end-testing.",
-								links: [
-									["Cypress", "https://www.cypress.io/"],
-									["End-to-End Testing with Cypress", "https://webdevstudios.com/2018/11/13/end-to-end-testing-with-cypress-io/"],
-								]
-							}
-						},
-						{
-							name: "Ava",
-							description: {
-								text: "Learn about the Ava test runner.",
-								links: [
-									["Github - Ava", "https://github.com/avajs/ava"],
-									["A Fresh Take On JavaScript Testing", "https://pusher.com/sessions/meetup/the-js-roundabout/ava-test-runner-a-fresh-take-on-javascript-testing-and-growing-an-open-source-project"],
-									["Freecodecamp - How you can test your Node.js applications", "https://www.freecodecamp.org/news/testing-your-nodejs-applications-with-ava-js-99e806a226a7/"],
-								]
-							},
-						}
-					]
-				},
-				{
-					name: "Best Practices",
-					description: {
-						text: "Learn about the best practices when it comes to testing.",
-						links: [
-							["Javascript Testing Best Practices", "https://github.com/goldbergyoni/javascript-testing-best-practices"],
-							["What are the best practices in software Testing?", "https://www.quora.com/What-are-the-best-practices-in-software-Testing"],
-							["7 Software Testing Best Practices", "https://www.qasymphony.com/blog/7-software-testing-best-practices/"],
+							["Emotion – introduction (eng)", "https://emotion.sh/docs/introduction"],
+							["What is System Testing?", "https://www.tutorialspoint.com/software_testing_dictionary/system_testing.htm"],
+							["System Testing", "http://softwaretestingfundamentals.com/system-testing/"],
 						]
 					}
 				}
