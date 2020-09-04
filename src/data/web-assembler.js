5d975b86a5c37fda3a1cec18f011b45a7e39fbc8
이시은 <te04056@naver.com>
2020-09-04 22:41:06 +0900

    "최종 아이콘 수정"

--- src/data/web-assembler.js
+++ src/data/web-assembler.js
@@ -8,10 +8,8 @@ export const webassemblerCollection = {
 					description: {
 						text: "웹 어셈블리는 Go, C, C++ 또는 Rust와 같은 고급 언어에서 생성된 이진 명령어다. 여기서 멈추지 말고 계속해서 배워나가야 할 것이다",
 						links: [
-							["MDN - Streams", "https://developer.mozilla.org/en-US/docs/Web/API/Streams_API"],
-							["MDN - Streams API concepts", "https://developer.mozilla.org/en-US/docs/Web/API/Streams_API/Concepts"],
-							["Experimenting with the Streams API", "https://deanhume.com/experimenting-with-the-streams-api/"],
-							["WHATWG - Streams", "https://streams.spec.whatwg.org/"],
+							["MDN - Streams", "hhttps://developer.mozilla.org/ko/docs/WebAssembly"],
+							["Assembly 개념 다지기", "https://www.joinc.co.kr/w/Site/Assembly/Documents/Assembly_Basic"],
 						]
 					}
 					}
