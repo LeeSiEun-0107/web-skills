7e59492ba3b5c99feeae1660d65cd113cd79150a
이시은 <te04056@naver.com>
2020-09-05 02:40:32 +0900

    merge확인 commit

--- src/data/cssarchitecture.js
+++ src/data/cssarchitecture.js
@@ -8,7 +8,7 @@ export const cssarchitectureCollection = {
 					description: {
 						links: [
 							["[CSS 방법론] BEM 방식", "https://nykim.work/15"],
-							["더 선호 -> BEM이란?", "https://junwoo45.github.io/2019-08-29-BEM/"]
+							["더 선호 -> BEM이란? ", "https://junwoo45.github.io/2019-08-29-BEM/"]
 						]
 					}
 				}
